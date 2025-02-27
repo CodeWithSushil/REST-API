973152eb042049a2710fbf8d2f1f55781d89dab2
Sushil Kumar <sushilkumbhar77@gmail.com>
2025-02-27 19:47:26 +0530

    add ajax request update

--- public/js/ajax.js
+++ public/js/ajax.js
@@ -1,18 +1,34 @@
-function ajax(method='GET', url, data=null){
+function ajax(method='GET', url, data=null)
+{
   const ajax = new XMLHttpRequest();
 
-  onreadystatechange = function() {
-    if(this.readyState == 4 && this.status == 200) {
-      document.getElementById('msg').innerText = this.responseText;
+  ajax.onreadystatechange = function()
+  {
+    if(
+      this.readyState == 4 &&
+      this.status == 200
+    )
+    {
+      document.getElementById('msg')
+        .innerHTML = this.responseText;
     }
   }
 
   ajax.(method, url, true);
 
-  if(method === "POST"){
-    ajax.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+  if(method === "POST")
+  {
+    ajax.setRequestHeader(
+      "Content-type",
+      "application/x-www-form-urlencoded"
+    );
+
+    const postData = JSON.stringify(data);
+    ajax.send(postData);
   }
-  ajax.send(data);
-  //console.log(ajax);
+  else
+  {
+    ajax.send();
+  }
 }
 
