3cb9bc1be270f37779e01d161837999ae99cd023
Sushil Kumar <sushilkumbhar77@gmail.com>
2025-02-27 14:23:23 +0530

    Database config

--- app/database.php
+++ app/database.php
@@ -3,14 +3,23 @@
 class Database {
   private $host = '127.0.0.1';
   private $port = 3306;
-  private $user = 'root';
-  private $pass = 'root';
+  private $username = 'root';
+  private $password = '';
   private $dbname = 'restapi';
   public $conn;
 
-  try {
-    //code...
-  } catch (\Throwable $th) {
-    //throw $th;
+  public function __construct(){
+    $this->conn = null;
+    
+    try {
+      $dns = "mysql:host={$this->host};port={$this->port};dbname={$this->dbname};";
+      $this->conn = new PDO($dns, $this->username, $this->password);
+      //$this->conn->setAttribute();
+    } catch (\Throwable $th) {
+      print($th->getMessage());
+      exit();
+    }
   }
 }
+
+$database = new Database();
